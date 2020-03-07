d04313d3adb8b6089b87ba28822659b70f64e270
sumit <sumiiitjsahu@gmail.com>
2020-03-07 15:43:39 +0530

    Attaching Line edit with circle

--- mainui.py
+++ mainui.py
@@ -1,10 +1,9 @@
 
-
 import sys, random
 from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QAction, QFileDialog
-from PyQt5.QtGui import QPainter, QPen, QImage, QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QAction, QFileDialog, QLineEdit
+from PyQt5.QtGui import QPainter, QPen, QImage, QPixmap, QStaticText, QColor
+from PyQt5.QtCore import Qt, QRect, QRectF, QPoint
 
 
 class Window(QMainWindow):
@@ -16,14 +15,13 @@ class Window(QMainWindow):
         self.height = 800
         self.title = "Create Circles"
         self.InitWindow()
-        
-        self.label = QLabel()
-        canvas = QPixmap(1000, 800)
-        canvas.fill()
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
 
-        self.circle = None
+        # self.label = QLabel()
+        # canvas = QPixmap(1000, 800)
+        # canvas.fill()
+        # self.label.setPixmap(canvas)
+        # self.setCentralWidget(self.label)
+        # self.circle = None
         # self.statusBar()
         self.circles = []
         self.show()
@@ -31,39 +29,29 @@ class Window(QMainWindow):
     def CreateMenu(self):
         mainMenu = self.menuBar()
 
-        self.adAction=QAction('Add',self)
+        self.adAction =QAction('Add' ,self)
         mainMenu.addAction(self.adAction)
         self.adAction.triggered.connect(lambda: self.drawCircle())
 
-        generate_reportAction=QAction('Generate Report',self)
+        generate_reportAction =QAction('Generate Report' ,self)
         mainMenu.addAction(generate_reportAction)
 
         saveAction = QAction('Save', self)
         mainMenu.addAction(saveAction)
         saveAction.triggered.connect(self.save_image)
 
-    def paintEvent(self, event):
-        if self.circle is not None:
-            painter = QPainter(self.label.pixmap())
-            painter.begin(self)
-            # print("called")
-            # self.circle.paint(painter)
-            painter.setPen(QPen(self.circle.color, 4, Qt.SolidLine))
-            x, y = self.circle.position.x(), self.circle.position.y()
-            painter.drawEllipse(x, y, self.circle.length, self.circle.length)
-            painter.end()
-            self.circle = None
-
     def drawCircle(self):
-        self.circle = Circle()
-        self.update()
-    
+        position = QtCore.QPoint(*random.choices(range(800), k=2))
+        b = Circle(self)
+        b.show()
+        b.move(position)
+
     def save_image(self):
         fileName = QFileDialog.getSaveFileName(self, 'some text', "image.png", '*.png')
         pixmap = self.label.pixmap()
         pixmap.save(fileName[0], "PNG")
 
-    
+
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
@@ -74,19 +62,24 @@ class Window(QMainWindow):
 
 
 
-class Circle():
-    def __init__(self):
-        self.color = QtGui.QColor(*random.choices(range(256), k=3))
-        self.position = QtCore.QPoint(*random.choices(range(800), k=2))
-        self.length = random.randrange(200)
-        self.textlable=QLabel()
+class Circle(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent=parent)
+        # self.setGeometry(30, 30, 600, 400)
+        self.setGeometry(QRect(0,0,200,200))
+        self.name = QLineEdit('cirA', self)
+        self.color = QColor(*random.choices(range(256), k=3))
+        # self.position = QtCore.QPoint(*random.choices(range(800), k=2))
+        self.position=QPoint(4,30)
+        self.length = 100
+        self.name=QLineEdit('cirA',self)
 
-    # def paint(self, painter):
-    #     painter.setPen(QPen(self.color,  4 , Qt.SolidLine))
-    #     x, y = self.position.x(), self.position.y()
-    #     painter.drawEllipse(x, y, self.length, self.length)
-        # print("should execute")
-
+    def paintEvent(self, event):
+            painter = QPainter(self)
+            # print("called")
+            painter.setPen(QPen(self.color, 4, Qt.SolidLine))
+            x, y = self.position.x(), self.position.y()
+            painter.drawEllipse(x, y, self.length, self.length)
 
 
 if __name__ == "__main__":
