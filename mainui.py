fe33af7b5e30ba5b43e465577705117d78d39dcd
sumit <sumiiitjsahu@gmail.com>
2020-03-08 18:10:58 +0530

    using graphview and delete selected circle

--- mainui.py
+++ mainui.py
@@ -1,9 +1,33 @@
-
 import sys, random
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QAction, QFileDialog, QLineEdit
-from PyQt5.QtGui import QPainter, QPen, QImage, QPixmap, QStaticText, QColor
+
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QAction, QFileDialog, QLineEdit, QGraphicsView,\
+    QMenu,QGraphicsScene, QGraphicsView, QGraphicsItem,QMenu
+from PyQt5.QtGui import QPainter, QPen, QImage, QPixmap, QStaticText, QColor, QCursor, QBrush
 from PyQt5.QtCore import Qt, QRect, QRectF, QPoint
+
+class GraphicsView(QGraphicsView):
+    def __init__(self, parent=None):
+        super(GraphicsView, self).__init__(parent)
+
+        self.setDragMode(QGraphicsView.RubberBandDrag)
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setRenderHint(QPainter.TextAntialiasing)
+
+    def contextMenuEvent(self, event):
+        scene=window.scene
+        list = scene.selectedItems()
+        contextMenu = QMenu()
+        deleteAction = contextMenu.addAction("Delete")
+        exitAction = contextMenu.addAction("Exit")
+        action = contextMenu.exec_(event.globalPos())
+        if action == deleteAction:
+            for item in list:
+                scene.removeItem(item)
+
+    def wheelEvent(self, event):
+        factor = 1.25 ** (-event.angleDelta().y()/ 240.0)
+        self.scale(factor, factor)
 
 
 class Window(QMainWindow):
@@ -16,15 +40,14 @@ class Window(QMainWindow):
         self.title = "Create Circles"
         self.InitWindow()
 
-        # self.label = QLabel()
-        # canvas = QPixmap(1000, 800)
-        # canvas.fill()
-        # self.label.setPixmap(canvas)
-        # self.setCentralWidget(self.label)
-        # self.circle = None
-        # self.statusBar()
-        self.circles = []
-        self.show()
+        self.graphicView = GraphicsView(self)
+        self.scene = QGraphicsScene(self)
+        self.scene.setSceneRect(0, 25, 1000, 800)
+
+
+        self.graphicView.setScene(self.scene)
+        self.graphicView.setGeometry(0, 25, 1000, 825)
+
 
     def CreateMenu(self):
         mainMenu = self.menuBar()
@@ -38,18 +61,11 @@ class Window(QMainWindow):
 
         saveAction = QAction('Save', self)
         mainMenu.addAction(saveAction)
-        saveAction.triggered.connect(self.save_image)
+        # saveAction.triggered.connect(self.save_image)
 
     def drawCircle(self):
-        position = QtCore.QPoint(*random.choices(range(800), k=2))
-        b = Circle(self)
-        b.show()
-        b.move(position)
-
-    def save_image(self):
-        fileName = QFileDialog.getSaveFileName(self, 'some text', "image.png", '*.png')
-        pixmap = self.label.pixmap()
-        pixmap.save(fileName[0], "PNG")
+        circle=Circle(self)
+        circle.drawCircle(self.scene)
 
 
     def InitWindow(self):
@@ -57,29 +73,37 @@ class Window(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.CreateMenu()
 
+    # def mousePressEvent(self, QMouseEvent):
+    #     # if QMouseEvent.button() == Qt.RightButton:
+    #     print("clicked right")
 
 
 
 
+class Circle(QtWidgets.QGraphicsEllipseItem):
+    def __init__(self, light, *args, **kwargs):
+        super(Circle, self).__init__(*args, **kwargs)
 
-class Circle(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent=parent)
-        # self.setGeometry(30, 30, 600, 400)
-        self.setGeometry(QRect(0,0,200,200))
-        self.name = QLineEdit('cirA', self)
         self.color = QColor(*random.choices(range(256), k=3))
-        # self.position = QtCore.QPoint(*random.choices(range(800), k=2))
-        self.position=QPoint(4,30)
+        self.position = QtCore.QPoint(*random.choices(range(800), k=2))
+        self.pen = QPen(self.color, 4, Qt.SolidLine)
         self.length = 100
-        self.name=QLineEdit('cirA',self)
 
-    def paintEvent(self, event):
-            painter = QPainter(self)
-            # print("called")
-            painter.setPen(QPen(self.color, 4, Qt.SolidLine))
-            x, y = self.position.x(), self.position.y()
-            painter.drawEllipse(x, y, self.length, self.length)
+    def drawCircle(self,scene):
+        x, y = self.position.x(), self.position.y()
+        ellipse = scene.addEllipse(x, y, 100, 100, self.pen, QBrush())
+        ellipse.setFlag(QGraphicsItem.ItemIsMovable)
+        ellipse.setFlag(QGraphicsItem.ItemIsSelectable)
+        ellipse.acceptHoverEvents()
+
+        # ellipse.acceptedMouseButtons()
+        textLabel = scene.addWidget(QLineEdit('cirA'))
+        textLabel.setGeometry(QRectF(x + 20, y - 25, 50, 10))
+        textLabel.setParentItem(ellipse)
+
+
+
+
 
 
 if __name__ == "__main__":
