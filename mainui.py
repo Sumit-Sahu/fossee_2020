78751a8a569cc4e6769da6fe04a36766ddfa2741
sumit <sumiiitjsahu@gmail.com>
2020-03-14 13:47:49 +0530

    create save as pdf method

--- mainui.py
+++ mainui.py
@@ -1,6 +1,7 @@
 import sys, random
 
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtPrintSupport import QPrinter
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QFileDialog, QLineEdit, QGraphicsView, \
     QMenu, QGraphicsScene, QGraphicsView, QGraphicsItem, QMenu, QGraphicsEllipseItem, QGraphicsLineItem, QAction, \
     QToolButton, QButtonGroup
@@ -9,6 +10,8 @@ from PyQt5.QtCore import Qt, QRect, QRectF, QPoint, QPointF, QLineF
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+
+
 
 from circle import Circle
 from connectingline import ConnectingLine
@@ -36,7 +39,6 @@ class Window(QMainWindow):
 
         self.view = QGraphicsView(self.scene)
         self.setCentralWidget(self.view)
-        # self.view.fitInView(self.scene.sceneRect())
 
     def pointerGroupClicked(self, i):
         Circle.myMode = self.connectionTypeGroup.checkedId()
@@ -48,7 +50,7 @@ class Window(QMainWindow):
 
         self.generateReportAction = QAction(QIcon('images/bringtofront.png'),
                                             "Generate Report", self, shortcut="Ctrl+G")
-        self.generateReportAction.triggered.connect(self.drawCircle)
+        self.generateReportAction.triggered.connect(self.saveAsPdf)
 
         self.saveAction = QAction(QIcon('images/bringtofront.png'),
                                   "Save", self, shortcut="Ctrl+G")
@@ -80,19 +82,73 @@ class Window(QMainWindow):
     def drawCircle(self):
         circle = Circle()
         circle.addOnCanvas(self.scene)
-        # self.scene.addItem(circle)
-        # circle.textLabel = self.scene.addWidget(QLineEdit('cirA'))
-        # circle.textLabel.setParentItem(circle)
-        # circle.textLabel.setGeometry(QRectF(circle.rect().x() + circle.rect().width() / 4,
-        #                                     circle.rect().y() - circle.pen().width() - 20,
-        #                                     50, 20))
-
 
     def saveAsPng(self):
-        fileName,_ = QFileDialog.getSaveFileName(self, 'Save file', "image.png", '*.png')
-        pixmap = QPixmap()
-        pixmap = self.view.grab()
-        pixmap.save(fileName, 'PNG')
+        try:
+            fileName, _ = QFileDialog.getSaveFileName(self, 'Save file', "image.png", '*.png')
+            if not fileName:
+                return
+            if QFileInfo(fileName).suffix() != '.png':  # check if user type other extension with name
+                fileName += '.png'
+            pixmap = self.view.grab()
+            pixmap.save(fileName, 'PNG')
+        except:
+            QMessageBox.information(self,'Error','Image not saved')
+
+    def saveAsPdf(self):
+        try:
+            from reportlab.pdfgen.canvas import Canvas
+            from reportlab.lib.pagesizes import A4
+            from reportlab.lib.units import inch
+            from reportlab.lib.colors import red, blue
+
+        except:
+            QMessageBox.information(self, 'Alert', 'Process Declined \n import reportlab')
+
+
+
+        items = self.scene.items()
+        fileName, _ = QFileDialog.getSaveFileName(self, directory='file.pdf',
+                                                  filter='PDF Files *.pdf(*.pdf);;All Files *(*)')
+        if not fileName:
+            return
+        print(QFileInfo(fileName).suffix())
+        if QFileInfo(fileName).suffix() != 'pdf':                                            # check if user type other extension with name
+            fileName += '.pdf'
+
+        canvas = Canvas(fileName, pagesize=A4)
+        canvas.setFont("Times-Roman", 30)
+        canvas.setFillColor(red)
+        canvas.drawCentredString(4 * inch, 11 * inch, "Connection Report")
+        canvas.setFillColor(blue)
+        size = 20
+        y = 10 * inch
+        x = 2 * inch
+        line = 1
+        for item in items:
+            if type(item) == ConnectingLine:
+                connectionLine = item.textLabel.text()
+                startCircle = item.getStartCircle().textLabel.text()
+                endCircle = item.getEndCircle().textLabel.text()
+                str = "{}:({},{})".format(connectionLine, startCircle, endCircle)
+
+                if line > 20:
+                    canvas.showPage()
+                    line = 4
+                    size = 7
+                    y = 10 * inch
+                    x = 2 * inch
+
+                canvas.setFont("Helvetica", size)
+                canvas.drawString(x, y, str)
+                line += 1
+                y = y - size * 2
+
+        try:
+            canvas.save()
+        except:
+            QMessageBox.information(self, 'Error', 'Error Generating Report \n'
+                                                   'first close file {}'.format(fileName))
 
 
 def main():
